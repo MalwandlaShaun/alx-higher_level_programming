937ceec3135bd4afa584c2cfe88ea8541a06d103
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-03-10 13:29:11 -0800

    init-final

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     from variable_load_5 import a
+    """printing the value of a from variable_load_5"""
+    from variable_load_5 import a
 
-     print(a)
+    print(a)
