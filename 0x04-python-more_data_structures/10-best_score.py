3666b22d88170a21dbe037b2afed4247a939f108
MalwandlaShaun <hlungwanimalwandlashaun@gmail.com>
2023-03-17 06:51:07 -0700

    2nd commit fix

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
+    if a_dictionary is None:
         return None
     max_score = max(a_dictionary.values())
-    for k,v in a_dictionary.items():
+    for k, v in a_dictionary.items():
         if v == max_score:
             return k
